1a3f06a4dd1ae95c8a3c5bd3f48a09d9ca0c8954
Kellye Greene <kellyecodes@gmail.com>
2018-02-28 10:43:15 -0500

    Cleanup document and add router

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,29 +1,17 @@
-// attempt to reset browser differences in default CSS
 require('normalize.css/normalize.css');
-// this file is where styles should go
 require('styles/App.css');
 require('styles/App.scss');
-
-// Flocabulary uses React extensively. This exercise is built with it
-// but it is not required knowledge. Ideally, you will not have to
-// alter any javascript. React comes with an HTML-like syntax
-// called jsx, which you can alter much like HTML.
 var React = require('react');
-
 var MadlibForm = require('./MadlibForm');
 var SubmittedMadlib = require('./SubmittedMadlib');
 
-// These are lyrics to a Flocabulary song that has had some
-// terms replaced for the purposes of the madlib.
 var MADLIB_TEXT = require('../madlibs/bill-of-rights');
 
-// This is the main component of the interface
 var AppComponent = React.createClass({
   render: function() {
     var content = (
       this.state.submittedValue
       ? (
-          // check out SubmittedMadlib.js to see the markup for this element
           <SubmittedMadlib
             reset={this.reset}
             text={this.props.text}
@@ -31,7 +19,6 @@ var AppComponent = React.createClass({
           />
         )
       : (
-          // check out MadlibForm.js to see the markup for this element
           <MadlibForm
             text={this.props.text}
             onSubmit={
